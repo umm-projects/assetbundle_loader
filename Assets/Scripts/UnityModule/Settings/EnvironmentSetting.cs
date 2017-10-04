4a2e63374ef5a7e7e37a120bcb5da5ca8c7c942a
Tetsuya Mori <monry84@gmail.com>
2017-10-04 17:48:34 +0900

    :sparkles: Add setting class
    * URL フォーマットと拡張子を定義可能にします

---
+++ Assets/Scripts/UnityModule/Settings/EnvironmentSetting.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace UnityModule.Settings {
+
+    public partial class EnvironmentSetting {
+
+        private const string DEFAULT_ASSET_BUNDLE_EXTENSION = ".unity3d";
+
+        [SerializeField][Tooltip("string.Format による置換が実行されます。\n{0}: AssetBundle 名\n{1}: アプリバージョン")]
+        private string assetBundleURLFormat;
+
+        public string AssetBundleURLFormat {
+            get {
+                return this.assetBundleURLFormat;
+            }
+            set {
+                this.assetBundleURLFormat = value;
+            }
+        }
+
+        [SerializeField]
+        private string assetBundleExtension = DEFAULT_ASSET_BUNDLE_EXTENSION;
+
+        public string AssetBundleExtension {
+            get {
+                return this.assetBundleExtension;
+            }
+            set {
+                this.assetBundleExtension = value;
+            }
+        }
+
+    }
+
+}
+
