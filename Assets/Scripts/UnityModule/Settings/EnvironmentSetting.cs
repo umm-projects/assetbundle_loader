b7b9ae34904125171df4d955631ca5da91459485
Tetsuya Mori <monry84@gmail.com>
2017-10-04 18:14:06 +0900

    :sparkles: Add field for Cache Path
    * Root AssetBundle をローカルにキャッシュする際のパスフォーマットを環境設定に追加

--- Assets/Scripts/UnityModule/Settings/EnvironmentSetting.cs
+++ Assets/Scripts/UnityModule/Settings/EnvironmentSetting.cs
@@ -4,6 +4,8 @@ namespace UnityModule.Settings {
 
     public partial class EnvironmentSetting {
 
+        private const string DEFAULT_ASSET_BUNDLE_CACHE_PATH_FORMAT = "AssetBundles/{0}";
+
         private const string DEFAULT_ASSET_BUNDLE_EXTENSION = ".unity3d";
 
         [SerializeField][Tooltip("string.Format による置換が実行されます。\n{0}: AssetBundle 名\n{1}: アプリバージョン")]
@@ -15,6 +17,18 @@ namespace UnityModule.Settings {
             }
             set {
                 this.assetBundleURLFormat = value;
+            }
+        }
+
+        [SerializeField][Tooltip("string.Format による置換が実行されます。\n{0}: AssetBundle 名\n{1}: アプリバージョン")]
+        private string assetBundleCachePathFormat = DEFAULT_ASSET_BUNDLE_CACHE_PATH_FORMAT;
+
+        public string AssetBundleCachePathFormat {
+            get {
+                return this.assetBundleCachePathFormat;
+            }
+            set {
+                this.assetBundleCachePathFormat = value;
             }
         }
 
